fef96d593470e3acf4a8267734d78a10511dbeba
anurag <anuragmahurkar99@gmail.com>
2022-07-02 00:18:34 +0530

    My first java code in git

---
+++ codingChallenge.java
@@ -0,0 +1,12 @@
+
+public class codingChallenge {
+
+  public void printString(){
+    System.out.println("Hello Great Learning");
+  }
+  public static void main(String[] args) {
+    codingChallenge obj1 = new codingChallenge();
+    obj1.printString();
+  }
+  
+}
